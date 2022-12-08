4e80fd4955db902be5b508018cdd491f97f1c07d
gloriahwoang <gloria.juhyun@gmail.com>
2022-12-08 04:29:03 -0500

    update index.js

--- interviewmaster-app/src/index.js
+++ interviewmaster-app/src/index.js
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import React from 'react'
-import Amplify from 'aws-amplify';
+import {Amplify} from 'aws-amplify';
 import config from './config';
 import App from './App';
 
