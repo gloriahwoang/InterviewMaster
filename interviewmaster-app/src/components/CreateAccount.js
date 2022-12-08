1c47a49e98875c4fd250e6fc352746109ead53b9
gloriahwoang <gloria.juhyun@gmail.com>
2022-12-08 00:51:31 -0500

    index, createaccount

--- interviewmaster-app/src/components/CreateAccount.js
+++ interviewmaster-app/src/components/CreateAccount.js
@@ -1,10 +1,78 @@
 import "./CreateAccountStyles.css";
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import FormErrors from "../FormErrors";
+import Validate from "../utility/FormValidation";
+import { Auth } from "aws-amplify";
 
+class CreateAccount extends Component {
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        confirmpassword: "",
+        errors: {
+          cognito: null,
+          blankfield: false,
+          passwordmatch: false
+        }
+      }
 
-const CreateAccount = () => {
-    return (
+      clearErrorState = () => {
+        this.setState({
+          errors: {
+            cognito: null,
+            blankfield: false,
+            passwordmatch: false
+          }
+        });
+      }
+
+      handleSubmit = async event => {
+        event.preventDefault();
+    
+        // Form validation
+        this.clearErrorState();
+        const error = Validate(event, this.state);
+        if (error) {
+          this.setState({
+            errors: { ...this.state.errors, ...error }
+          });
+        }
+
+        // AWS Cognito integration here
+        const { username, email, password } = this.state;
+        try {
+        const signUpResponse = await Auth.signUp({
+            username,
+            password,
+            attributes: {
+            email: email
+            }
+        });
+        this.props.history.push("/welcome");
+        console.log(signUpResponse);
+        } catch (error) {
+        let err = null;
+        !error.message ? err = { "message": error } : err = error;
+        this.setState({
+            errors: {
+            ...this.state.errors,
+            cognito: err
+            }
+        });
+        }
+    }
+
+    onInputChange = event => {
+        this.setState({
+          [event.target.id]: event.target.value
+        });
+        document.getElementById(event.target.id).classList.remove("is-danger");
+      }
+    
+    render() {
+      return (
         <div className='create-background'>
             <div className='create'>
                 <h1 className="create-title">Create a New Account</h1>
@@ -14,24 +82,49 @@ const CreateAccount = () => {
                 
                 
                 <div className="auth-form-container">
-                <form className="create-form">
-                    <label for='fullname'>Full Name</label>
-                    <input type='fullname' placeholder='John Doe' id='fullname' name='fullname'/>
+                <form className="create-form" onSubmit={this.handleSubmit}>
+                    <label for='username'>Username</label>
+                    <input 
+                        type='username'
+                        placeholder='John Doe' 
+                        id='username' 
+                        aria-describedby="userNameHelp"
+                        value={this.state.username}
+                        onChange={this.onInputChange}
+                    />
                     <label for='email'>Email</label>
-                    <input type='email' placeholder='youremail@gmail.com' id='email' name='email'/>
+                    <input 
+                        type='email'
+                        placeholder='youremail@gmail.com'
+                        id='email'
+                        aria-describedby="emailHelp"
+                        value={this.state.email}
+                        onChange={this.onInputChange}
+                    />
                     <label for='password'>Password</label>
-                    <input type='password' placeholder='**********' id='password' name='password'/>
-                    <Link to = "/sync"><button className="create-button">Join InterviewMaster</button></Link>
+                    <input
+                    type='password'
+                    placeholder='**********'
+                    id='password'
+                    value={this.state.password}
+                    onChange={this.onInputChange}
+                    />
+                    <label for='coonfirmpassword'>Confirm Password</label>
+                    <input
+                    type='password'
+                    placeholder='**********'
+                    id='confirmpassword'
+                    value={this.state.confirmpassword}
+                    onChange={this.onInputChange}
+                    />
+                    <button className="create-button">Join InterviewMaster</button>
                 </form>
                     <p>By submitting the form you agree to InterviewMaster's <a href="..">Terms of Service</a></p>
                 </div>
                
             </div>
         </div>
-
-
-
-    );
-};
-
-export default CreateAccount;
+        );
+      };
+}
+    export default CreateAccount;
--- interviewmaster-app/src/index.js
+++ interviewmaster-app/src/index.js
@@ -1,9 +1,19 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import React from 'react'
-
-
+import Amplify from 'aws-amplify';
+import config from './config';
 import App from './App';
+
+Amplify.configure({
+    Auth: {
+        mandatorySignId: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    }
+});
+
 
 const rootElement= document.getElementById('root');
 const root=createRoot(rootElement);
