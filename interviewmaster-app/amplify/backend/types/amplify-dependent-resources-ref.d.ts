ca022a2a3305f7fdccadc0f24a0712ce5fd431f9
gloriahwoang <gloria.juhyun@gmail.com>
2022-12-08 23:25:56 -0500

    amplify full stack - install

---
+++ interviewmaster-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "interviewmasterappe675e7ad": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "interviewmasterapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
