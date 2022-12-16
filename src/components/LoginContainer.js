ca4f335f6e261859f928124be688d226adb0d74c
aaronsxzhao <sz2940@columbia.edu>
2022-12-16 13:24:12 -0500

    aaronz
    aaronz

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -40,9 +40,10 @@ class LoginContainer extends Component {
     // AWS Cognito integration here
     try {
       const user = await Auth.signIn(this.state.username, this.state.password);
-      console.log(user);
+      this.props.auth.setAuthStatus(true);
+      this.props.auth.setUser(user);
       this.props.history.push("/myinterviews");
-      window.location.reload();
+      // window.location.reload();
       
     } catch(error) {
       let err = null;
