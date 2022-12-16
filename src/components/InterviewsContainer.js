dda3705e6e4a6b8e98ea873f2847410bc4b4f164
aaronsxzhao <sz2940@columbia.edu>
2022-12-16 13:44:17 -0500

    aaronz
    aaronz

--- src/components/InterviewsContainer.js
+++ src/components/InterviewsContainer.js
@@ -1,47 +1,49 @@
 import "./InterviewsContainerStyles.css";
 import InterviewExcelLine from "../components/InterviewExcelLine";
-import React from 'react'
-
-// const apigClient = apigClientFactory.newClient();
+import React from 'react';
 
 class InterviewsContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {inputList: []};
-        this.handleclick = this.handleclick.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { inputList: [] };
+    this.handleclick = this.handleclick.bind(this);
+  }
 
-    handleclick = () => {
-        const inputList = this.state.inputList;
-        this.setState({
-            inputList: inputList.concat(<InterviewExcelLine/>)
-        });
-    }
-    render() {
+  handleclick = () => {
+    const inputList = this.state.inputList;
+    this.setState({
+      inputList: inputList.concat(<InterviewExcelLine auth={this.props.auth} />)
+    });
+  };
+
+  render() {
     return (
-        <div className="Container">
-            <h1>Your Interviews</h1>
-            <ul>
-                <li>Company</li>
-                <li>Position</li>
-                <li>Status</li>
-                <li>Referral</li>
-                <li>Date</li>
-                <li>Location</li>
-                <li>Referrer</li>
-                <li>LinkedIn</li>
-            </ul>
-            <InterviewExcelLine/>
-            <InterviewExcelLine />
-            {this.state.inputList.map(function (input, index) {
-                    return input;  
-                })}
-            <button onClick={this.handleclick} className="addbutton">+</button>
-                
-        </div>
-        
+      <div className="Container">
+        <h1>Your Interviews</h1>
+        <ul>
+          <li>Company</li>
+          <li>Position</li>
+          <li>Status</li>
+          <li>Referral</li>
+          <li>Date</li>
+          <li>Location</li>
+          <li>Referrer</li>
+          <li>LinkedIn</li>
+        </ul>
+        <InterviewExcelLine auth={this.props.auth} />
+        {this.state.inputList.map((input, index) => (
+          <InterviewExcelLine auth={this.props.auth} key={index} />
+        ))}
+        <InterviewExcelLine auth={this.props.auth} />
+        {this.state.inputList.map((input, index) => (
+          <InterviewExcelLine auth={this.props.auth} key={index} />
+        ))}
+        <button onClick={this.handleclick} className="addbutton">
+          +
+        </button>
+      </div>
     );
-    }
-};
+  }
+}
 
 export default InterviewsContainer;
